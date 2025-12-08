e16ba368df8e1851024121402479b079240dc8dc
Mahadevan Reji <rmahadevan574@gmail.com>
2025-12-09 00:59:58 +0530

    refactor: update gsap import statement for consistency

--- app/components/Stack/Stack.tsx
+++ app/components/Stack/Stack.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useRef } from "react";
-import { gsap } from "gsap";
+import gsap  from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { TextPlugin } from "gsap/TextPlugin";
 import styles from "./Stack.module.css";
